607face0c8b83cef3e0b7f77d8cc0ee8740c92d0
nazmul <nazmulhasan0290@gmail.com>
2022-11-22 12:14:42 +0600

    modal

---
+++ client/components/Home/Modal.tsx
@@ -0,0 +1,17 @@
+const Modal = () => {
+  const handleClick = (e: any) => {
+    console.log(e.target.matches(".modal"));
+  };
+  return (
+    <div
+      className="h-screen w-full bg-black/25 fixed top-0 left-0 z-20"
+      onClick={handleClick}
+    >
+      <div className="h-[100px] bg-white modal w-[515px] mx-auto">
+        This is modal content
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import BiggestPost from "../components/Home/BiggestAd";
 import CategoryLocation from "../components/Home/CategoryLocation";
 import Heading from "../components/Home/Heading";
+import Modal from "../components/Home/Modal";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
           <div className="border w-[515px]">
             <CategoryLocation />
             <BiggestPost />
+            <Modal />
           </div>
           <div className="border w-[210px]">Advertisement</div>
         </div>
