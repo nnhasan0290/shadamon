3caccf13a3d4ed0c0294f3319fec0cc77ee51a18
nhasan <nazmulhasan02901@gmail.com>
2022-12-01 16:28:51 +0600

    account-page

--- client/components/Home/account/Account.tsx
+++ client/components/Home/account/Account.tsx
@@ -1,21 +1,40 @@
 import Dashboard from "./Dashboard";
 import Profile from "./Profile";
+import { useState } from "react";
 
 export default function () {
+  const [pageState, setPageState] = useState("DASHBOARD");
   return (
     <div className="w-full bg-body-bg">
       <div className="flex self-start justify-start gap-2 p-2 bg-white  rounded-md align-start text-[13.5px] border-b shadow-lg">
-        <h2 className="px-2 py-1 font-semibold rounded-md bg-common-color">
+        <h2
+          className={`px-2 cursor-pointer  py-1 font-semibold rounded-md transition-all duration-300  ${
+            pageState === "DASHBOARD"
+              ? "bg-common-color text-white"
+              : "bg-account-gray"
+          }`}
+          onClick={() => {
+            setPageState("DASHBOARD");
+          }}
+        >
           Dashboard
         </h2>
-        <h2 className="px-2 py-1 font-semibold rounded-md bg-account-gray">
+        <h2
+          className={`px-2 py-1 transition-all duration-300 font-semibold rounded-md  cursor-pointer ${
+            pageState === "PROFILE"
+              ? "bg-common-color text-white"
+              : "bg-account-gray"
+          }`}
+          onClick={() => setPageState("PROFILE")}
+        >
           Profile
         </h2>
         <h2 className="px-2 py-1 font-semibold rounded-md bg-account-gray">
           Settings
         </h2>
       </div>
-      <Profile />
+      {pageState === "DASHBOARD" && <Dashboard />}
+      {pageState === "PROFILE" && <Profile />}
     </div>
   );
 }
