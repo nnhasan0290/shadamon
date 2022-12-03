6a1b746e37ce8729c5f276a7feee86cae7868e3b
work0290 <nazmulwork0290@gmail.com>
2022-12-03 00:48:03 +0000

    select-ant

--- client/components/Home/account/Profile.tsx
+++ client/components/Home/account/Profile.tsx
@@ -2,6 +2,7 @@ import { BiMinus, BiSave } from "react-icons/bi";
 import { BsPlus } from "react-icons/bs";
 import { HiPhotograph } from "react-icons/hi";
 import { useRef } from "react";
+import SellerInfo from "./SellerInfo";
 
 export default function () {
   const cvRef = useRef<HTMLInputElement>(null);
@@ -362,6 +363,7 @@ export default function () {
               </button>
             </div>
           </form>
+          <SellerInfo/>
         </div>
       </div>
     </div>
