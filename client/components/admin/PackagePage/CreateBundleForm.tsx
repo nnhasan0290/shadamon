fbe48a337f8632ac6c6561615e325b76024a5285
nazmul02902 <nazmulhasan02902@gmail.com>
2023-01-02 00:37:44 +0000

    main

--- client/components/admin/PackagePage/CreateBundleForm.tsx
+++ client/components/admin/PackagePage/CreateBundleForm.tsx
@@ -1,5 +1,6 @@
-import { TextField } from "@mui/material";
+import { InputLabel, MenuItem, TextField } from "@mui/material";
 import {
+  Button,
   Divider,
   Form,
   Input,
@@ -8,12 +9,21 @@ import {
   Table,
   Typography,
 } from "antd";
+import { Select as MuiSelect } from "@mui/material";
 import { BiPlus } from "react-icons/bi";
 import EachPackageTable from "./EachPackageTable";
+import { useState } from "react";
 const { Option } = Select;
 
 export default function ({ record }: any) {
   const [form] = Form.useForm();
+  const [average, setAverage] = useState("");
+
+  //muit select change
+  const muiAverageChange = (event: any) => {
+    setAverage(event.target.value as string);
+  };
+
   return (
     <Form form={form} className="flex flex-wrap justify-between basis-full">
       <div className="flex gap-2 items-center font-bold">
@@ -51,19 +61,44 @@ export default function ({ record }: any) {
           </div>
           <div className="flex">
             <Form.Item name={"post"} className="w-[50px]">
-              <TextField size="small" id="outlined-basic" label="Sell" variant="outlined"/>
+              <TextField
+                size="small"
+                id="outlined-basic"
+                label="Sell"
+                variant="outlined"
+              />
             </Form.Item>
             <Form.Item name={"post"} className="w-[50px]">
-              <TextField size="small" id="outlined-basic" label="bid" variant="outlined"/>
+              <TextField
+                size="small"
+                id="outlined-basic"
+                label="bid"
+                variant="outlined"
+              />
             </Form.Item>
             <Form.Item name={"post"} className="w-[50px]">
-              <TextField size="small" id="outlined-basic" label="Post" variant="outlined"/>
+              <TextField
+                size="small"
+                id="outlined-basic"
+                label="Post"
+                variant="outlined"
+              />
             </Form.Item>
             <Form.Item name={"post"} className="w-[50px]">
-              <TextField size="small" id="outlined-basic" label="Post" variant="outlined"/>
+              <TextField
+                size="small"
+                id="outlined-basic"
+                label="Post"
+                variant="outlined"
+              />
             </Form.Item>
             <Form.Item name={"post"} className="w-[50px]">
-              <TextField size="small" id="outlined-basic" label="Post" variant="outlined"/>
+              <TextField
+                size="small"
+                id="outlined-basic"
+                label="Post"
+                variant="outlined"
+              />
             </Form.Item>
           </div>
           <div>
@@ -71,6 +106,98 @@ export default function ({ record }: any) {
             <EachPackageTable />
             <EachPackageTable />
           </div>
+          <Typography>Checked Feature Write-up</Typography>
+          <div className="flex">
+            <Form.Item>
+              <Input placeholder="checked 1" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="checked 2" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="checked 3" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="checked 4" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="checked 5" />
+            </Form.Item>
+          </div>
+          <Typography>Un Checked Feature Write-up</Typography>
+          <div className="flex">
+            <Form.Item>
+              <Input placeholder="checked 1" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="checked 2" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="checked 3" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="checked 4" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="checked 5" />
+            </Form.Item>
+          </div>
+          <div className="flex gap-1">
+            <Form.Item>
+              <TextField
+                value={average}
+                className="w-[180px]"
+                onChange={(e) => setAverage(e.target.value)}
+                select // tell TextField to render select
+                label="Average/Cata wise"
+              >
+                <MenuItem key={1} value="test">
+                  Test 1
+                </MenuItem>
+                <MenuItem key={2} value="test2">
+                  Test 2
+                </MenuItem>
+              </TextField>
+            </Form.Item>
+            <Form.Item name={""}>
+              <TextField
+                id="outlined-basic"
+                label="Best Suggestion"
+                variant="outlined"
+              />
+            </Form.Item>
+            <Form.Item name={""}>
+              <TextField
+                id="outlined-basic"
+                label="Note"
+                variant="outlined"
+              />
+            </Form.Item>
+            <Form.Item name={""}>
+              <TextField
+                id="outlined-basic"
+                label="Order"
+                variant="outlined"
+              />
+            </Form.Item>
+            <Form.Item>
+              <TextField
+                className="w-[100px]"
+                select // tell TextField to render select
+                label="Status"
+              >
+                <MenuItem key={1} value="test">
+                  Test 1
+                </MenuItem>
+                <MenuItem key={2} value="test2">
+                  Test 2
+                </MenuItem>
+              </TextField>
+            </Form.Item>
+          </div>
+          <Form.Item className="">
+            <Button htmlType="submit" className="w-full  bg-green-700  text-white">Save</Button>
+          </Form.Item>
         </div>
         <div className="basis-1/2"></div>
       </div>
